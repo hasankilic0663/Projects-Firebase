7850b6f3aa9b3d03bc69b629f6f2da2c3d4d5fa0
Hasan Hüseyin KILIÇ <hasankilic0663@hotmail.com>
2024-11-04 09:55:32 +0300

    Selector App Created

---
+++ SelectorApp/SelectorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelectorApp
+//
+//  Created by Hasan Hüseyin Kılıç on 4.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
