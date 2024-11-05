c256e41d38891b1e8c60e5dd9e3261a7bd4f75c4
Hasan Hüseyin KILIÇ <hasankilic0663@hotmail.com>
2024-11-05 16:58:23 +0300

    SelectorApp DatePicker try

--- SelectorApp/SelectorApp/ViewController.swift
+++ SelectorApp/SelectorApp/ViewController.swift
@@ -1,19 +1,37 @@
 //
-//  ViewController.swift
-//  SelectorApp
-//
-//  Created by Hasan Hüseyin Kılıç on 4.11.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet weak var yearPicker: UIDatePicker!
+    @IBOutlet weak var selectedYearLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //setupYearPicker()
+        updateYearLabel(with: yearPicker.date)
+    }
+    
+    private func setupYearPicker() {
+        // Sadece yıllar arasında seçim yapılabilmesi için minimum ve maksimum tarihleri ayarla
+        let calendar = Calendar.current
+        var components = DateComponents()
+        components.year = -100 // 100 yıl önceye kadar
+        yearPicker.minimumDate = calendar.date(byAdding: components, to: Date())
+        components.year = 0 // Şimdiki yıl
+        yearPicker.maximumDate = calendar.date(byAdding: components, to: Date())
     }
 
-
+    // Tarih değiştiğinde çağrılacak action
+    @IBAction func yearChanged(_ sender: UIDatePicker) {
+        updateYearLabel(with: sender.date)
+    }
+    
+    private func updateYearLabel(with date: Date) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy" // Sadece yılı göster
+        selectedYearLabel.text = "Seçilen Yıl: \(formatter.string(from: date))"
+    }
 }
-
