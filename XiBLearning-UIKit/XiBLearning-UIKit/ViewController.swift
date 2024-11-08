75691971ff71e7b8ca8ca63909795dadaf9c6b77
Hasan Hüseyin KILIÇ <hasankilic0663@hotmail.com>
2024-11-08 11:39:10 +0300

    XiB Learning Created

---
+++ XiBLearning-UIKit/XiBLearning-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XiBLearning-UIKit
+//
+//  Created by Hasan Hüseyin Kılıç on 8.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XiBLearning/XiBLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XiBLearning
+//
+//  Created by Hasan Hüseyin Kılıç on 8.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
